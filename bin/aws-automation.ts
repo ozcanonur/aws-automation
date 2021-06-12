23e103ed7935a6c26aa0432c0beb2099761cf1c3
Onur <onur@9fin.com>
2021-06-12 22:03:15 +0100

    Init

--- bin/aws-automation.ts
+++ bin/aws-automation.ts
@@ -15,7 +15,7 @@ new AwsAutomationStack(app, 'AwsAutomationStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '006917775159', region: 'eu-west-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
