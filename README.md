d517809e0a9cc3ac2436d3a1a6ba5a06ca54a324
Onur <onur@9fin.com>
2021-06-13 00:44:53 +0100

    Automate Fargate deployment

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# Welcome to your CDK TypeScript project!
+# Welcome to your CDK Fargate automation!
 
-This is a blank project for TypeScript development with CDK.
-
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+The `cdk.json` file tells the CDK Toolkit how to execute the app.
 
 ## Useful commands
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+
+## .env
+
+- ACCOUNT=
+- REGION=
+- APP_NAME=NAME_TO_PREFIX_AWS_SERVICES
+- REPO_NAME=ECR_REPO_NAME
+- CONTAINER_PORT=DOCKER_INSTANCES_LISTEN_ON
+- DESIRED_COUNT=ECS_SERVICE_TASK_COUNT
--- lib/aws-automation-stack.ts
+++ lib/aws-automation-stack.ts
@@ -1,9 +1,79 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecr from '@aws-cdk/aws-ecr';
+import * as elb from '@aws-cdk/aws-elasticloadbalancingv2';
 
-export class AwsAutomationStack extends cdk.Stack {
+export class AutomateFargate extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dockerRepositoryName = process.env.REPO_NAME || '';
+    const appName = process.env.APP_NAME || '';
+    const containerPort = parseInt(process.env.CONTAINER_PORT || '80');
+    const desiredCount = parseInt(process.env.DESIRED_COUNT || '2');
+
+    const defaultVpc = ec2.Vpc.fromLookup(this, 'vpc-lookup', {
+      isDefault: true,
+    });
+
+    // Create cluster
+    const cluster = new ecs.Cluster(this, 'cluster', {
+      clusterName: `${appName}-cluster`,
+      vpc: defaultVpc,
+    });
+
+    // Create task definitions
+    const taskDefinition = new ecs.TaskDefinition(this, 'task-definition', {
+      family: `${appName}-task-definition`,
+      compatibility: ecs.Compatibility.FARGATE,
+      memoryMiB: '0.5GB',
+      cpu: '.25 vCPU',
+    });
+
+    const repo = ecr.Repository.fromRepositoryName(this, 'repository', dockerRepositoryName);
+    taskDefinition.addContainer('container', {
+      containerName: `${appName}-container`,
+      image: ecs.ContainerImage.fromEcrRepository(repo, 'latest'),
+      memoryLimitMiB: 350,
+      portMappings: [
+        {
+          containerPort,
+        },
+      ],
+    });
+
+    // Create service
+    const service = new ecs.FargateService(this, 'service', {
+      cluster,
+      taskDefinition,
+      serviceName: `${appName}-service`,
+      desiredCount,
+      minHealthyPercent: 0,
+      maxHealthyPercent: 100,
+      enableECSManagedTags: true,
+      assignPublicIp: true,
+    });
+
+    // Create load balancer
+    const loadBalancer = new elb.ApplicationLoadBalancer(this, 'lb', {
+      loadBalancerName: `${appName}-lb`,
+      vpc: defaultVpc,
+      internetFacing: true,
+    });
+
+    const listener = loadBalancer.addListener('Listener', {
+      port: 80,
+    });
+
+    // Connect lb with service
+    service.registerLoadBalancerTargets({
+      containerName: `${appName}-container`,
+      containerPort,
+      newTargetGroupId: `${appName}-target`,
+      listener: ecs.ListenerConfig.applicationListener(listener, {
+        protocol: elb.ApplicationProtocol.HTTP,
+      }),
+    });
   }
 }
